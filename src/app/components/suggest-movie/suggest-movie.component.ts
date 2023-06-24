b20fb4ff329908d20bdfb0843d330e43960b9b6e
KnightCrawler3 <manishbarasker@gmail.com>
2023-06-25 02:02:10 +0530

    dynamic loading of wall image

--- src/app/components/suggest-movie/suggest-movie.component.ts
+++ src/app/components/suggest-movie/suggest-movie.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { delay } from 'rxjs/internal/operators/delay';
@@ -6,15 +7,28 @@ import { MoviesService } from 'src/app/service/movies.service';
 @Component({
   selector: 'app-suggest-movie',
   templateUrl: './suggest-movie.component.html',
-  styleUrls: ['./suggest-movie.component.scss']
+  styleUrls: ['./suggest-movie.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
+      transition('* => void', [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]),
+    ])
+  ]
 })
-export class SuggestMovieComponent {
+export class SuggestMovieComponent implements OnInit {
 
   public showTVMovieBtn: boolean = true;
   public genreslist: any;
   private _selectedFeature;
   private _selectedList = new Map();
+  public nowPlaying;
+  current=1;
   constructor(private _movie: MoviesService, private router: Router) { }
+
+  ngOnInit() {    
+    this.trendingMovies(1);
+    this.sliderTimer();
+  }
 
   btnClick(feature) {
     this.showTVMovieBtn = false;
@@ -36,5 +50,17 @@ export class SuggestMovieComponent {
     const querystring = Array.from(this._selectedList.keys()).join(",");
     const route = this._selectedFeature == 'movie' ? '/genres' : '/genres-tv';
     this.router.navigateByUrl(`${route}/${querystring}`);
+  }
+
+  trendingMovies(page: number) {
+    this._movie.getNowPlaying(page).pipe(delay(2000)).subscribe((res: any) => {
+      this.nowPlaying = res.results;
+    });
+  }
+
+  sliderTimer() {
+    setInterval(() => {
+      this.current = ++this.current % this.nowPlaying.length;
+    }, 5000);
   }
 }
